a33863443ba30cb669dc58720ec32d60db70efdf
Milesq <wmilosz88@gmail.com>
2019-01-06 22:19:35 +0100

    listing dir to tree

--- listdir.py
+++ listdir.py
@@ -1,5 +1,13 @@
 from os import listdir as ls
-from os.path import isdir
+from os.path import isdir, join
 
 def listdir(path='./'):
 	ret = {}
+	for i in ls(path):
+		absPath = join(path, i)
+		if isdir(absPath):
+			ret[absPath] = listdir(absPath)
+		else:
+			ret[absPath] = None
+
+	return ret

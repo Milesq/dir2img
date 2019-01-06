32464cad37f71d1076e2697dcd99435db6a679b6
Milesq <wmilosz88@gmail.com>
2019-01-06 22:05:25 +0100

    initial commit

---
+++ listdir.py
@@ -0,0 +1,5 @@
+from os import listdir as ls
+from os.path import isdir
+
+def listdir(path='./'):
+	ret = {}
---
+++ main.py
@@ -0,0 +1,9 @@
+from listdir import listdir
+
+import os
+os.system('cls')
+
+mypath = 'C:/Users/wmilo/Desktop/'
+mypath = './'
+
+listdir()
